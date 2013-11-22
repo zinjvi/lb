97c5d6d302c5739d32d4bdfb086e2027d7cb0d23
vitaliy-zinchenko <vitaliy.zinchenko90@gmail.com>
2013-11-22 08:55:01 +0200

    commit

--- scripts/app/forum/js.js
+++
@@ -1,5 +0,0 @@
-define(function(){
-    return{
-        f: 123
-    }
-});
--- scripts/app/forum/views/base.js
+++
@@ -1,8 +0,0 @@
-define(['use!backbone', 'use!dust'], function (Backbone, dust) {
-    var BaseView = Backbone.View.extend({
-        renderTemplate: function () {
-            this.templateName;
-        }
-    });
-    return BaseView;
-});
