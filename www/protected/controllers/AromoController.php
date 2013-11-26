b6cae87de4667117f15fa62adc2a2d0e6b620347
vitaliy-zinchenko <vitaliy.zinchenko90@gmail.com>
2013-11-26 18:35:41 +0200

    c

--- www/protected/controllers/AromoController.php
+++ www/protected/controllers/AromoController.php
@@ -10,9 +10,11 @@ class AromoController extends Controller
         $rows[] = array('id'=>1, 'title'=>'ttttttt1');
         $rows[] = array('id'=>2, 'title'=>'ttttttt2');
         $rows[] = array('id'=>3, 'title'=>'ttttttt3');
-        Yii::log(CJSON::encode($rows));
-        header('Content-Type: application/json');
-        echo CJSON::encode($rows);
+
+        header('content-type:text/x-json');
+        header('cache-control:no-store,no-cache,max-age=0,must-revalidate');
+        echo json_encode($rows);
+//        Yii::app->end();
     }
 
 }
--- www/scripts/app/aromo/collections/AromoCollection.js
+++ www/scripts/app/aromo/collections/AromoCollection.js
@@ -1,12 +1,23 @@
 define(['use!backbone', 'aromo/models/AromoModel'], function(Backbone, AromoModel){
+    var self;
     var AromoCollection = Backbone.Collection.extend({
         model: AromoModel,
         url: '/aromo/list',
         initialize: function(){
+            self = this;
             this.add([
                 {'id': 1, 'name': 'tt1', 'text':'asdasdsa asdasdas asdasdad'},
                 {'id': 2, 'name': 'tt2', 'text':'hgf htf fytfky lkuf ff'}
             ]);
+            this.l();
+        },
+        l: function(){
+            this.fetch({
+                async: false,
+                success: function(collection, response) {
+                    self.models = collection;
+                }
+            });
         }
     });
     return AromoCollection;
