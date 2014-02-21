bfff30401daf9b46540bd57856bb243ac0faa4fb
vitaliy-zinchenko <vitaliy.zinchenko90@gmail.com>
2014-02-21 19:56:27 +0200

    c

--- src/itest/java/zinchenko/rest/ITArticleRestApiTest.java
+++ src/itest/java/zinchenko/rest/ITArticleRestApiTest.java
@@ -6,8 +6,11 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.response.Response;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -25,11 +28,27 @@ import static org.junit.Assert.assertEquals;
  * Date: 16.02.14
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration({"classpath:src/test/resources/hibernate-applicationContext.xml"})
-@DatabaseSetup("src/test/resources/dataset.xml")
+@ContextConfiguration({"classpath:zinchenko/hibernate-applicationContext.xml"})
+@DatabaseSetup("classpath:zinchenko/dataset.xml")
 @TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
         DbUnitTestExecutionListener.class})
 public class ITArticleRestApiTest {
+
+    @Autowired
+    SessionFactory sessionFactory;
+
+    @Test
+    public void test() {
+        try {
+            Session session = sessionFactory.openSession();
+            session.createCriteria(Article.class).list();
+            System.out.println("");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        System.out.println("t");
+    }
 
     @Test
     public void testGetById() throws Exception {
@@ -68,13 +87,13 @@ public class ITArticleRestApiTest {
     public void testDelete() throws Exception {
         List<Article> articles = new ArrayList<Article>();
         Article article = new Article();
-        article.setId(1L);
-        article.setTitle("test title 1");
-        article.setDescription("test description 1");
+        article.setId(2L);
+        article.setTitle("test title 2");
+        article.setDescription("test description 2");
         articles.add(article);
         String expectedBody = new ObjectMapper().writeValueAsString(articles);
 
-        Response response = RestAssured.delete(TestConstants.ARTICLE_PATH + "/2");
+        Response response = RestAssured.delete(TestConstants.ARTICLE_PATH + "/1");
         assertEquals(200, response.getStatusCode());
 
         String all = RestAssured.get(TestConstants.ARTICLE_PATH + "/all").asString();
---
+++ src/main/java/zinchenko/S.java
@@ -0,0 +1,33 @@
+package zinchenko;
+
+import org.h2.tools.Server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class S {
+
+    public static void main(String[] args) throws SQLException {
+//        TcpServer service = new TcpServer();
+//        service.init("-tcpAllowOthers", "-tcpPort", "9099");
+//        service.start();
+
+//        Server embeddedServer = new Server();
+//        embeddedServer.run(new String[] { "-tcp", "-web" }, System.out);
+//        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", " sa", "");
+
+        System.out.println("Starting Web Server.......");
+//        Server webSrv = Server.createWebServer(new String[]{"-trace"}).start();
+        Server webSrv = Server.createTcpServer("-tcpPort", "9123", "-tcpAllowOthers").start();
+//        webSrv.init("-tcpAllowOthers", "-tcpPort", "9099");
+//        webSrv.start();
+        System.out.println("Started Web Server.......");
+
+        Connection conn = DriverManager.getConnection("jdbc:h2:tcp://localhost:9099/~/my", "my", "");
+
+        webSrv.stop();
+
+    }
+
+}
