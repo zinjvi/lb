d9ff5f1125cd8ae94fed89b23d5618373945f67e
zinjvi <zinjvi@gmail.com>
2014-02-27 23:39:36 +0200

    c

--- src/main/java/zinchenko/rest/impl/ArticleRestApiImpl.java
+++ src/main/java/zinchenko/rest/impl/ArticleRestApiImpl.java
@@ -1,5 +1,7 @@
 package zinchenko.rest.impl;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import zinchenko.dao.ArticleDao;
@@ -7,12 +9,15 @@ import zinchenko.domain.Article;
 import zinchenko.rest.ArticleRestApi;
 import zinchenko.rest.Bean;
 import zinchenko.service.ArticleService;
+import zinchenko.service.UnexpectedServiceException;
 
 import javax.ws.rs.core.Response;
 import java.util.List;
 
 //TODO | provide presentation server errors correctly
 public class ArticleRestApiImpl implements ArticleRestApi {
+
+    private static final Log LOG = LogFactory.getLog(ArticleRestApiImpl.class);
 
 //    @Autowired
 //    Bean bean;
@@ -34,8 +39,14 @@ public class ArticleRestApiImpl implements ArticleRestApi {
     @Override
     @Transactional
     public List<Article> getAll() {
-        List<Article> articles = articleDao.findAll();
-        return articles;
+        try {
+            List<Article> articles = articleDao.findAll();
+            LOG.debug(articles);
+            return articles;
+        } catch (Exception e) {
+            LOG.error("Fail loading all articles", e);
+            throw new UnexpectedServiceException(e);
+        }
     }
 
     @Override
